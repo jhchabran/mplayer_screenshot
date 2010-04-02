f4f7f35337e3bf504ff4e514f27d744580cf255d
JH. Chabran <jh@chabran.fr>
2010-04-02 16:10:45 +0200

    added more specs

--- spec/mplayer_screenshot_spec.rb
+++ spec/mplayer_screenshot_spec.rb
@@ -6,7 +6,20 @@ describe "MplayerScreenshot" do
   end
   
   it "should take a screenshot at the 25th second" do
-    MPlayer::Screenshoter.new(@test_video).take(:at => 25, :filename => 'test.png')
+    MPlayer::Screenshoter.new(@test_video, 'mplayer').take(:at => 25, :filename => 'test.png')
     File.exists?('test.png').should be_true
+  end
+  
+  it "should provide a take method through Screenshot module" do
+    MPlayer::Screenshot.take(@test_video, :at => 20, :filename => 'test.png')
+    File.exists?('test.png').should be_true
+  end
+  
+  it "should raise some errors if :at is missing" do
+    lambda {MPlayer::Screenshot.take(@test_video, :filename => 'test.png')}.should raise_error
+  end
+  
+  it "should raise some errors if :filename is missing" do
+    lambda {MPlayer::Screenshot.take(@test_video, :at => 4)}.should raise_error
   end
 end
